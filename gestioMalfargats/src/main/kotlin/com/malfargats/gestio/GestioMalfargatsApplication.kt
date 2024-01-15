44e174a88f66f9bfdd266aa4168f7352acdb0e83
joanpaucg <joanpau.castells@hotmail.com>
2024-01-15 21:21:05 +0100

    Initial commit Initial Spring boot configurtion Kotlin and Postgres

---
+++ gestioMalfargats/src/main/kotlin/com/malfargats/gestio/GestioMalfargatsApplication.kt
@@ -0,0 +1,11 @@
+package com.malfargats.gestio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GestioMalfargatsApplication
+
+fun main(args: Array<String>) {
+	runApplication<GestioMalfargatsApplication>(*args)
+}
---
+++ gestioMalfargats/src/test/kotlin/com/malfargats/gestio/GestioMalfargatsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.malfargats.gestio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GestioMalfargatsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
